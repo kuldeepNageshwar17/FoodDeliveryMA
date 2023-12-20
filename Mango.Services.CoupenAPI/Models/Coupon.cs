7bee8922338228c1482867841bae61b8dabb7852
Kuldeep Nageshwar <35416870+kuldeepNageshwar17@users.noreply.github.com>
2023-12-20 10:37:15 +0530

    initial commit with database changes

---
+++ Mango.Services.CoupenAPI/Models/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
+{
+	public class Coupon
+	{
+		[Key]
+		public int  CuponId { get; set; }
+		[Required] 
+		public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+		public int MinAmount { get; set; }
+		public DateTime LastUpdated { get; set; }
+		public DateTime CreatedDate { get; set; }
+
+	}
+}
+
