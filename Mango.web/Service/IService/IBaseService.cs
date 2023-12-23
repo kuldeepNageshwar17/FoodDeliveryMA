68c40d20a85c37032a149333e83e15ad2dcb0a89
Kuldeep Nageshwar <35416870+kuldeepNageshwar17@users.noreply.github.com>
2023-12-23 14:19:18 +0530

    Base service implementation for api call

---
+++ Mango.web/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Mango.web.Models;
+
+namespace Mango.web.Service.IService
+{
+    public interface IBaseService {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }	
+}
+
---
+++ Mango.web/Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Mango.web.Utility
+{
+	public class SD
+	{
+		public enum ApiType
+		{
+			GET,
+			POST,
+			PUT,
+			DELETE
+		}
+
+	}
+}
+
